3e2bb2c1447b056b6f126f4853d6f4352b56eb8d
Philip Knott <filupnot@proton.me>
2026-08-10 21:11:53 -0700

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Public values, safe to commit and safe to ship in the deployed JS.
 // Fill these in after you create the Lambda Function URL and the VAPID keypair (see SETUP.md).
 window.UB_CONFIG = {
-  FUNCTION_URL: 'https://REPLACE_WITH_FUNCTION_URL.lambda-url.us-west-2.on.aws',
-  VAPID_PUBLIC_KEY: 'REPLACE_WITH_VAPID_PUBLIC_KEY',
+  FUNCTION_URL: 'https://blha24z5jecz6tgwwqrxgcf3sm0tfuiv.lambda-url.us-west-2.on.aws',
+  VAPID_PUBLIC_KEY: 'BNIQDQOZGdPrtTkLkw_57j4xImgaYdzJHAgkVurgosBlW2F8MLyd0I1MyFUKjeWSOjDc9_2UV8r2EdLlDA40xcE',
 };
